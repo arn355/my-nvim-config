995508de998f644262174edcb27d5bb0cc54c380
Arn <86312780+arn355@users.noreply.github.com>
2023-01-19 09:21:32 +0700

    update autocomplete setting

--- lua/plugins/configs/nvim-cmp.lua
+++ lua/plugins/configs/nvim-cmp.lua
@@ -33,9 +33,10 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
     ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-    -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- select = true : pressing enter (<CR>) automatically uses the first recommendation
-    ["<Tab>"] = cmp.mapping.confirm({ select = true }), 
+        
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+    -- select = true : pressing enter or tab (<CR> or <Tab>) automatically uses the first recommendation
   }),
 
   -- sources for autocompletion
